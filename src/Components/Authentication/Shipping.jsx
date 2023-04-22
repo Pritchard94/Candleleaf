e2707bc4e128fb4194fb2e4b18958e1c82acc1f8
Pritchard Oluwafeyisanumi <104890172+Pritchard94@users.noreply.github.com>
2023-04-22 12:24:35 +0100

    shipping

--- src/Components/Authentication/Shipping.jsx
+++ src/Components/Authentication/Shipping.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import Button from '../Button';
+import {Link} from 'react-router-dom'
 
 const Shipping = () => {
   const initialValues = {
